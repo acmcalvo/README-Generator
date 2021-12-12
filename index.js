d3409e18bfb907b33457306a8e36c633ec1d394f
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2021-12-12 12:35:52 -0500

    WIF

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
-const generatePage = require('./src/page-template');
-const { writeFile, copyFile } = require('./utils/generate-site');
+const generatePage = require('./src/page-template.js');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 
 const promptUser = () => {
   return inquirer.prompt([
