972d79d2250d7ddaad46eb8d640b9747275bfcc3
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2021-12-19 16:38:44 -0500

    WIP

--- index.js
+++ index.js
@@ -137,8 +137,8 @@ const questions = () => {
 
 // function to write README file using file system 
 const writeFile = data => {
-  // fs.writeFile('./dist/README.md', data, err => {
-    fs.writeFile('README.md', data, err => {
+  fs.writeFile('./dist/README.md', data, err => {
+    // fs.writeFile('README.md', data, err => {
       // if there is an error 
       if (err) {
           console.log(err);
